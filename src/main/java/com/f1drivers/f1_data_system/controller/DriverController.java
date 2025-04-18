1ae30d3a2758102e298fd68812f171cab152c433
E-Albert <magicspoon123@gmail.com>
2025-04-17 21:36:25 -0400

    added method to retrieve all drivers from database in controller

--- src/main/java/com/f1drivers/f1_data_system/controller/DriverController.java
+++ src/main/java/com/f1drivers/f1_data_system/controller/DriverController.java
@@ -4,9 +4,12 @@ package com.f1drivers.f1_data_system.controller;
 import com.f1drivers.f1_data_system.model.Driver;
 import com.f1drivers.f1_data_system.repository.DriverRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //this controller manipulates the data in the database
 @RestController
@@ -20,5 +23,12 @@ public class DriverController {
     @PostMapping("/driver")
     Driver newDriver(@RequestBody Driver newDriver) {
         return driverRepository.save(newDriver);
+    }
+
+    //reading data
+    @GetMapping("/drivers") //path
+    List<Driver> getAllDrivers() {
+        //method given by JPA
+        return driverRepository.findAll();
     }
 }
